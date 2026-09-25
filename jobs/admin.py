e43b982451af60ac815a15ff683bd97aead5bedd
Rahul Mohanty <rahulmohanty008@gmail.com>
2026-09-25 14:53:53 -0400

    Add Job and Application models and admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Job, Application
 
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'company', 'city', 'work_mode']
+    list_filter = ['work_mode', 'visa_sponsorship']
+    search_fields = ['title', 'skills', 'company']
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(Application)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    WORK_MODES = [
+        ('On-site', 'On-site'),
+        ('Remote', 'Remote'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    description = models.TextField()
+    skills = models.TextField()
+    city = models.CharField(max_length=255)
+    salary_min = models.IntegerField(default=0)
+    salary_max = models.IntegerField()
+    work_mode = models.CharField(max_length=255, choices=WORK_MODES, default='On-site')
+    visa_sponsorship = models.BooleanField(default=False)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.title
+
+
+class Application(models.Model):
+    STATUSES = [
+        ('Applied', 'Applied'),
+        ('Review', 'Review'),
+        ('Interview', 'Interview'),
+        ('Offer', 'Offer'),
+        ('Closed', 'Closed'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE) #delete all job object and the User object related to this. 
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note = models.TextField()
+    status = models.CharField(max_length=255, choices=STATUSES, default='Applied')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + ' → ' + self.job.title
