1c075d20dbd43477ea8fbb7c4baca19ebe573acc
Devin Yue <devin.y0411@gmail.com>
2026-09-24 20:02:47 -0400

    login and profile model implementation

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,53 @@
-from django.shortcuts import render
-
 from jobs.views import jobs
-
-
+from django.shortcuts import render
+from .forms import CustomUserCreationForm, CustomErrorList
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('home.index')
 def login(request):
     template_data = {}
-    template_data['title'] = 'Log in'
-    return render(request, 'accounts/login.html', {'template_data': template_data})
-
-
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            template_data['error'] = 'The username or password is incorrect.'
+            return render(request, 'accounts/login.html',
+                {'template_data': template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
 def signup(request):
     template_data = {}
-    template_data['title'] = 'Sign up'
-    return render(request, 'accounts/signup.html', {'template_data': template_data})
-
+    template_data['title'] = 'Sign Up'
+    if request.method == 'GET':
+        template_data['form'] = CustomUserCreationForm()
+        return render(request, 'accounts/signup.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
+        if form.is_valid():
+            user = form.save()
+            role = request.POST.get('role', Profile.SEEKER)
+            if role not in (Profile.SEEKER, Profile.RECRUITER):
+                role = Profile.SEEKER
+            Profile.objects.create(user=user, role=role)
+            return redirect('accounts.login')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html', {'template_data': template_data})
 
 def profile(request):
     template_data = {}
